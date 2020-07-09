108d66f61db303c5f9d32188784ae142152358c9
taycurran <taylorcurranagtc@gmail.com>
2020-07-09 16:43:10 -0500

    Practicing Mapping

--- game/cards.py
+++ game/cards.py
@@ -31,8 +31,8 @@ class FaceCard(Card):
     def _points(self) -> Tuple[int, int]:
         return 10, 10
 
-# Factory Function
-def card(rank: int, suit: Suit) -> Card:
+# Factory Function Example A
+def cardA(rank: int, suit: Suit) -> Card:
     if rank == 1:
         return AceCard("A", suit)
     elif 2 <= rank < 11:
@@ -43,6 +43,47 @@ def card(rank: int, suit: Suit) -> Card:
     raise Exception("Design Failure")
 
 # Whoah... double list comprehension... trippy
-deck = [card(rank, suit) 
-    for rank in range(1,14) 
+deckA = [cardA(rank, suit) 
+        for rank in range(1,14) 
         for suit in iter(Suit)] 
+
+# The above comprehension is equivalent to the following loop.
+def understand_cmp():
+    deck = []
+    for rank in range(1, 14):
+        for suit in iter(Suit):
+            dck.append(cardA(rank, suit))
+    return deck
+
+# Factory Function Example B
+
+# We can map from the rank parameter to the class that must be constructed
+
+# --- Understand Dict.get() to Understand how CardB works... ---
+# Return Value from get()
+# The get() method returns:
+
+# the value for the specified key if key is in dictionary.
+# None if the key is not found and value is not specified.
+# -- value if the key is not found and value is specified. --
+
+d = {1: 11, 2: 22, 3: 33}
+assert d[2] == 22
+assert d.get(2) == 22
+assert d.get(4) == None
+assert d.get(4, d[3]+11) == 44
+
+
+def cardB(rank: int, suit: Suit) -> Card:
+    # The Card value is specified in the get 
+    # statement if the rank is not found.
+    class_ = {1: AceCard, 11: FaceCard, 12: FaceCard,
+                13: FaceCard}.get(rank, Card)
+    return class_(str(rank), suit)
+
+deckB = [cardB(rank, suit) 
+        for rank in range(1,14) 
+        for suit in iter(Suit)] 
+
+# The cardB() function, however, has a serious deficiency. 
+# It lacks the translation from 1 to A and 13 to K that we had in previous versions.
